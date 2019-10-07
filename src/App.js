f56f15be0f59ba72f9f43708df911b6a043da9af
sirmcclay <mcclay.design@gmail.com>
2019-10-07 05:13:08 -0300

    Topcis - 3, 4, 5 - still studying...

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+import profile from './assets/Laughting Man - Tratado.jpg';
+
+function App() {
+  return <img width="200" src={profile} />
+}
+
+export default App;
