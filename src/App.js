62b44cbb12de8578df0d12357304fef6222056e4
sirmcclay <mcclay.design@gmail.com>
2019-10-08 04:48:06 -0300

    Topcis - 6 - continue...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/Laughting Man - Tratado.jpg';
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="200" src={profile} />
+  return <TechList />
 }
 
 export default App;
