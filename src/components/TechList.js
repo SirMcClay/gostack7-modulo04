8d5743a066e100af69513b55af303094fea5880b
sirmcclay <mcclay.design@gmail.com>
2019-10-11 04:38:24 -0300

    Topcis - 7 - finish...next...

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -11,17 +11,31 @@ class TechList extends Component {
   };
 
   handleInputChange = e => {
-    console.log(e.target.value);
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
   }
   
   render() {
     return (
-      <>
+      <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
         </ul>
-        <input type="text" onChange={this.handleInputChange} />
-      </>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
