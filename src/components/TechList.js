991ebcb4e4b58925af0d45946952ba93a92f43e2
sirmcclay <mcclay.design@gmail.com>
2019-10-16 05:30:02 -0300

    Topcis - 10, 11 - continue...

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,12 +5,22 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+  
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -33,7 +43,13 @@ class TechList extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
+          {this.state.techs.map(tech => (
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
         </ul>
         <input 
           type="text" 
